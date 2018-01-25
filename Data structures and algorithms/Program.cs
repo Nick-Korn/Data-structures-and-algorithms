6f9586e2669a2a509e8b8c63d7794e0ed301a0f3
Niko Liimatainen <K8908@Labranet.jamk.fi>
2018-01-25 12:38:37 +0200

    Finnished assignments 1-3 of set 1

--- Data structures and algorithms/Program.cs
+++ Data structures and algorithms/Program.cs
@@ -10,6 +10,62 @@ namespace Data_structures_and_algorithms
     {
         static void Main(string[] args)
         {
+            //Numbers(13);
+            //Console.WriteLine(Fibo(5));
+            //Console.WriteLine(Number(3));
+            Console.WriteLine(Reku(int.Parse(Console.ReadLine())));
         }
+
+        //Assignment 1 
+        static void Numbers(int b)
+        {
+            int k = 0;
+            List<int> a = new List<int>();
+            a.Add(1);
+
+            while (a.Count() <= b)
+            {
+                k = k + 1;
+                a.Add(a[k - 1] + a[k - 1]);
+            }
+
+            foreach (var c in a)
+            {
+                Console.WriteLine(c);
+            }
+        }
+
+        static int Fibo(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine(n);
+                return Fibo(n - 2) + Fibo(n - 1);
+            }
+        }
+
+        static int Number(int n)
+        {
+            Console.WriteLine(n);
+            return Fibo(n - 1) / Fibo(n);
+        }
+
+        static int Reku(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine(n);
+                return 1 / (1 + Reku(n - 1));
+            } 
+        }   
     }
 }
+
