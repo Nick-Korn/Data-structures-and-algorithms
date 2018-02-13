a462888d7603535403399d256d70099a747571b7
Niko Liimatainen <K8908@LABRANET.JAMK.FI>
2018-02-13 13:43:17 +0200

    Startetd assignment 4 of set 2

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -19,21 +19,26 @@ namespace Assignment2
             {
                 Console.WriteLine(k);
             }
-            */
-            /* 
+
             int[] values = new int[8] {50, 70, 30, 2, 40, 24, 19, 60};
             int[] results = InsertionSort(values);
             foreach (int num in results)
             {
                 Console.WriteLine(num);
             }
-            */
+            
             List<int> Values = new List<int>(){9,3,11,2};
             List<int> result = Bubble(Values);
             foreach (int number in result)
             {
                 Console.WriteLine(number);
             }
+            */
+            List<int> temp = new List<int>();
+            int start = 1;
+            int end = temp.count;
+            List<int> Values = new List<int>(){60, 170, 30, 2, 11, 19, 24, 50, 60, 3, 100};
+            MergeSort(Values, temp, start, end);
         }
 
         static int[] Shuffle(List<int> v, List<int> key)
@@ -85,6 +90,53 @@ namespace Assignment2
                 }
             }
             return values;
+        }
+        static List<int> MergeSort(List<int> values, List<int> temp, int start, int end)
+        {
+
+            if (start >= end) break;
+            else if (start + 1 == end)
+	        {
+                if(t[start] < t[end])
+                {
+                    int temp = temp[end];
+                    temp[end] = temp[start];
+                    temp[start] = temp[end];
+                }
+	        }
+            else 
+            {
+                middle = (start + end)/2;
+                MergeSort(values, temp, start, middle);
+                MergeSort(values, temp, middle + 1, end);
+            }
+            i = alku;
+            j = middle + 1;
+            k = start;
+
+            while (k <= end)
+            {
+                if (i > middle)
+                {
+                    while (j <= end)
+                    {
+                        l[k] = t[j];
+                        k = k + 1;
+                        j = j + 1;
+                    }
+
+                }
+
+                else if (j > end)
+                {
+                    while (i <= middle)
+                    {
+                        l[k] = t[i];
+                        k = k + 1;
+                        i = i + 1;
+                    }
+                }
+            }
         }
     }
 }
